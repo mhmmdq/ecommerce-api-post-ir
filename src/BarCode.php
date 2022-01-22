6212ecba83d22c4beee2688d366fd0111fcaca73
mhmmdq <mhmmdq@mhmmdq.ir>
2022-01-22 08:24:28 +0330

    Receive barcode output statically

--- src/BarCode.php
+++ src/BarCode.php
@@ -8,12 +8,8 @@ class BarCode {
     public const CODE128B_START_BASE = 104;
     public const CODE128C_START_BASE = 105;
     public const STOP = 106;
-    public function __construct( string $barcode )
-    {
-        echo $this->PngToBase64( $barcode );
-    }
 
-    public function code128BarCode ( $code , $density = 1 ) {
+    protected static function  code128BarCode ( $code , $density = 1 ) {
 
         $code128_bar_codes = array(212222, 222122, 222221, 121223, 121322, 131222, 122213, 122312, 132212, 221213, 221312, 231212, 112232, 122132, 122231, 113222, 123122, 123221, 223211, 221132, 221231,213212, 223112, 312131, 311222, 321122, 321221, 312212, 322112, 322211, 212123, 212321, 232121, 111323, 131123, 131321, 112313, 132113, 132311, 211313, 231113, 231311,112133, 112331, 132131, 113123, 113321, 133121, 313121, 211331, 231131, 213113, 213311, 213131, 311123, 311321, 331121, 312113, 312311, 332111, 314111, 221411, 431111,111224, 111422, 121124, 121421, 141122, 141221, 112214, 112412, 122114, 122411, 142112, 142211, 241211, 221114, 413111, 241112, 134111, 111242, 121142, 121241, 114212,124112, 124211, 411212, 421112, 421211, 212141, 214121, 412121, 111143, 111341, 131141, 114113, 114311, 411113, 411311, 113141, 114131, 311141, 411131, 211412, 211214,211232, 23311120);
         $width = (((11 * strlen($code)) + 35) * ($density/72));
@@ -41,12 +37,16 @@ class BarCode {
         return $img;
     }
 
-    public function PngToBase64( $barcode ) 
+    protected static function PngToBase64( $barcode ) 
     {
         ob_start();
-        imagepng($this->code128BarCode( $barcode ));
+        imagepng(self::code128BarCode( $barcode ));
         return "data:image/png;base64," . base64_encode(ob_get_clean());
     }
 
+    public static function make( string $BarCode )
+    {
+        return self::PngToBase64( $BarCode );
+    }
 }
 
